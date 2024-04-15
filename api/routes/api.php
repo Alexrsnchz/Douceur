8b16f077800b50063ba7181a4c954916cc036d4e
Alexrsnchz <alexrsnchz@gmail.com>
2024-04-15 23:06:48 +0200

    Douceur | Api | Rutas Api

---
+++ api/routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\AddressController;
+use App\Http\Controllers\AllergenController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::apiResource('addresses', AddressController::class);
+Route::apiResource('users', UserController::class);
+Route::apiResource('posts', PostController::class);
+
+Route::apiResource('allergens', AllergenController::class);
+Route::apiResource('categories', CategoryController::class);
+Route::apiResource('products', ProductController::class);
