b7040764dd64b495d33b0790d8a58f4f64e9584e
Alexrsnchz <alexrsnchz@gmail.com>
2024-04-20 23:47:24 +0200

    Douceur | Front | Contacto: Arreglo

--- front/src/components/contacto/FindUs.jsx
+++ front/src/components/contacto/FindUs.jsx
@@ -9,28 +9,32 @@ function FindUs() {
         <img
           src={contacto_banner}
           alt="Banner de la página"
-          className="brightness-50"
+          className="brightness-50 w-full"
         />
-        <div className="absolute inset-0 flex flex-col items-center justify-center text-white">
-          <h1 className="text-4xl lg:text-6xl text-center font-bold shadow-lg p-4 bg-opacity-40 rounded-lg">
-            Visita nuestras tiendas
-          </h1>
-          <p className="mt-4 text-xl lg:text-2xl text-center">
-            Encuéntranos en tu ciudad más cercana
-          </p>
+        <div className="absolute inset-0 flex flex-col items-center justify-center text-white px-4">
+          <div className="absolute inset-0 flex flex-col items-center justify-center">
+            <h1 className="mt-4 sm:mt-6 text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-center font-bold shadow-lg bg-opacity-40 rounded-lg flex items-center">
+              Visita nuestras tiendas
+            </h1>
+            <p className="hidden sm:block mt-1 sm:mt-3 text-xs sm:text-sm md:text-base lg:text-lg text-center">
+              Encuéntranos en tu ciudad más cercana
+            </p>
+          </div>
         </div>
       </div>
-      <div className="grid grid-cols-3">
+      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3">
         <div className="bg-[#FFE4C4] text-[#333333] p-4 flex flex-col items-center justify-center">
-          <div className="text-center py-5">
+          <div className="text-center py-3 lg:py-5">
             <img
               src={shop_icon}
               alt="Icono de tienda"
-              className="w-10 h-auto mx-auto mb-4"
+              className="w-10 h-auto mx-auto mb-3 lg:mb-4"
             />
             <div>
-              <span className="font-semibold">Douceur Almería</span>
-              <p className="text-[#333333]">
+              <span className="font-semibold text-lg lg:text-xl">
+                Douceur Almería
+              </span>
+              <p className="text-[#333333] text-sm lg:text-base">
                 <Link
                   to="https://maps.app.goo.gl/xoGeRkvBmH5jNDKX7"
                   target="_blank"
@@ -41,29 +45,41 @@ function FindUs() {
                 </Link>
               </p>
             </div>
-            <hr className="border-[#333333] my-2 w-full" />
+            <hr className="border-[#333333] my-2 lg:my-3 w-full" />
             <div>
-              <span className="font-semibold text-[#333333]">Horario</span>
-              <p className="text-[#333333]">Lunes a Viernes</p>
-              <p className="text-[#6B7280]">9:00 - 13:30 | 17:30 - 20:00</p>
+              <span className="font-semibold text-sm lg:text-base">
+                Horario
+              </span>
+              <p className="text-[#333333] text-xs lg:text-sm">
+                Lunes a Viernes
+              </p>
+              <p className="text-[#6B7280] text-xs lg:text-sm">
+                9:00 - 13:30 | 17:30 - 20:00
+              </p>
             </div>
-            <hr className="border-[#333333] my-2 w-full" />
+            <hr className="border-[#333333] my-2 lg:my-3 w-full" />
             <div>
-              <span className="font-semibold text-[#333333]">Teléfono</span>
-              <p className="text-[#333333]">+34 612 945 172</p>
+              <span className="font-semibold text-sm lg:text-base">
+                Teléfono
+              </span>
+              <p className="text-[#333333] text-xs lg:text-sm">
+                +34 612 945 172
+              </p>
             </div>
           </div>
         </div>
         <div className="bg-[#E8C7A7] text-[#333333] p-4 flex flex-col items-center justify-center">
-          <div className="text-center py-5">
+          <div className="text-center py-3 lg:py-5">
             <img
               src={shop_icon}
               alt="Icono de tienda"
-              className="w-10 h-auto mx-auto mb-4"
+              className="w-10 h-auto mx-auto mb-3 lg:mb-4"
             />
             <div>
-              <span className="font-semibold">Douceur Roquetas de Mar</span>
-              <p className="text-[#333333]">
+              <span className="font-semibold text-lg lg:text-xl">
+                Douceur Roquetas de Mar
+              </span>
+              <p className="text-[#333333] text-sm lg:text-base">
                 <Link
                   to="https://maps.app.goo.gl/ZCcuCvUao35ZR3717"
                   target="_blank"
@@ -74,29 +90,41 @@ function FindUs() {
                 </Link>
               </p>
             </div>
-            <hr className="border-[#333333] my-2 w-full" />
+            <hr className="border-[#333333] my-2 lg:my-3 w-full" />
             <div>
-              <span className="font-semibold text-[#333333]">Horario</span>
-              <p className="text-[#333333]">Lunes a Viernes</p>
-              <p className="text-[#6B7280]">9:30 - 13:00 | 16:30 - 20:00</p>
+              <span className="font-semibold text-sm lg:text-base">
+                Horario
+              </span>
+              <p className="text-[#333333] text-xs lg:text-sm">
+                Lunes a Viernes
+              </p>
+              <p className="text-[#6B7280] text-xs lg:text-sm">
+                9:30 - 13:00 | 16:30 - 20:00
+              </p>
             </div>
-            <hr className="border-[#333333] my-2 w-full" />
+            <hr className="border-[#333333] my-2 lg:my-3 w-full" />
             <div>
-              <span className="font-semibold text-[#333333]">Teléfono</span>
-              <p className="text-[#333333]">+34 672 239 019</p>
+              <span className="font-semibold text-sm lg:text-base">
+                Teléfono
+              </span>
+              <p className="text-[#333333] text-xs lg:text-sm">
+                +34 672 239 019
+              </p>
             </div>
           </div>
         </div>
         <div className="bg-[#FFDAB9] text-[#333333] p-4 flex flex-col items-center justify-center">
-          <div className="text-center py-5">
+          <div className="text-center py-3 lg:py-5">
             <img
               src={shop_icon}
               alt="Icono de tienda"
-              className="w-10 h-auto mx-auto mb-4"
+              className="w-10 h-auto mx-auto mb-3 lg:mb-4"
             />
             <div>
-              <span className="font-semibold">Douceur Aguadulce</span>
-              <p className="text-[#333333]">
+              <span className="font-semibold text-lg lg:text-xl">
+                Douceur Aguadulce
+              </span>
+              <p className="text-[#333333] text-sm lg:text-base">
                 <Link
                   to="https://maps.app.goo.gl/oe932FC4DdRYuVH68"
                   target="_blank"
@@ -107,16 +135,26 @@ function FindUs() {
                 </Link>
               </p>
             </div>
-            <hr className="border-[#333333] my-2 w-full" />
+            <hr className="border-[#333333] my-2 lg:my-3 w-full" />
             <div>
-              <span className="font-semibold text-[#333333]">Horario</span>
-              <p className="text-[#333333]">Lunes a Viernes</p>
-              <p className="text-[#6B7280]">9:00 - 14:00 | 17:30 - 20:00</p>
+              <span className="font-semibold text-sm lg:text-base">
+                Horario
+              </span>
+              <p className="text-[#333333] text-xs lg:text-sm">
+                Lunes a Viernes
+              </p>
+              <p className="text-[#6B7280] text-xs lg:text-sm">
+                9:00 - 14:00 | 17:30 - 20:00
+              </p>
             </div>
-            <hr className="border-[#333333] my-2 w-full" />
+            <hr className="border-[#333333] my-2 lg:my-3 w-full" />
             <div>
-              <span className="font-semibold text-[#333333]">Teléfono</span>
-              <p className="text-[#333333]">+34 646 576 237</p>
+              <span className="font-semibold text-sm lg:text-base">
+                Teléfono
+              </span>
+              <p className="text-[#333333] text-xs lg:text-sm">
+                +34 646 576 237
+              </p>
             </div>
           </div>
         </div>
